d73b1e634c832c33fcd82fe308627a61eee67493
Iegler <PedroIegler1601@gmail.com>
2023-01-30 14:25:37 -0300

    update

--- static/assets/js/sendNMesa.js
+++ static/assets/js/sendNMesa.js
@@ -13,8 +13,7 @@ $(function(){
 
     if(noticeTable > 0){
         var mesaServiced = $("#"+idMesa+"");
-        mesaServiced.css("background-color","rgb(179 22 22)");
-        mesaServiced.css("border-bottom","14px solid rgb(53 53 53)");
+        mesaServiced.addClass("atendida");
     }
 
 });
