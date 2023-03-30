573567f5e4079db3c83f8e1cdfb9ed928ecd7ebc
PedroHIegler <PedroIegler1601@gmail.com>
2023-03-30 20:03:31 -0300

    update

--- static/assets/js/core/user/transitionScreen.js
+++ static/assets/js/core/user/transitionScreen.js
@@ -1,11 +1,34 @@
 $(function(){
 
     showMenu();
+    showBox();
 
     function showMenu(){
         $("#icon-menu").click(function(){
             $(".menu-perfil-mobile nav").slideToggle(100);
         });
+    }
+
+    function showBox(){
+
+        verifyButton();
+
+        $(".esqueci-codigo").click((e) => {
+            e.stopPropagation();
+            $(".esqueciSenha").fadeIn(200);
+        });
+
+        function verifyButton(){
+            var aux = $("body, .iconClose");
+
+            aux.click(function(){
+                $('.esqueciSenha').fadeOut(100);
+            });
+    
+            $('.boxSenha').click(function(e){
+                e.stopPropagation();
+            });
+        }    
     }
     
 });
